849c26e1f5b3f72191ba4a3487afbeaf86740e15
Arstin Reano <arstingabrelreano@gmail.com>
2024-12-19 13:23:58 +0800

    Recipe model validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,12 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :recipe_type, presence: true
+  validates :description, presence: true
+  validates :steps, presence: true
+  validates :nutrition_rating, presence: true, 
+            numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :prep_time, presence: true
+
+  # If steps is stored as a string, serialize it as an array
+  serialize :steps, Array
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :recipes, only: [:index, :create]
 end
