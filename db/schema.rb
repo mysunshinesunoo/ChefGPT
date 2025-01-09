6cd513a606115b8be8a2840acddaf6d284fed4ef
mysunshinesunoo <caracajes95@gmail.com>
2025-01-09 19:52:55 +0800

    adding tailwind

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_07_023716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
@@ -53,6 +60,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_07_023716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
   add_foreign_key "ingredients", "users"
   add_foreign_key "recipes", "users"
 end
