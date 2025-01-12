d61e9cf8c0e4a5176e0f7a5c07df0aa68f7749d3
Arstin Reano <arstingabrelreano@gmail.com>
2025-01-12 13:00:06 +0800

    Added Home page stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_user_stats, if: :user_signed_in?
+
+  private
+
+  def set_user_stats
+    @user_stats = {
+      recipes: current_user.recipes.count,
+      favorites: current_user.recipes.where(is_favorite: true).count,
+      ingredients: current_user.ingredients.count
+    }
+  end
 end
